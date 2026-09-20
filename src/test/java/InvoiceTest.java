bf832e575258e268757c1dcaa95022901850a0e6
Hi7ro <a.k_windsor@protonmail.com>
2026-09-20 17:49:03 +0200

    project cleanup

--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -14,13 +14,6 @@ public class InvoiceTest {
   }
 
   @Test
-  void invoiceDueYesterdayIsOverdue() {
-    Invoice invoice = new Invoice("Test 2", 300.0, LocalDate.now().minusDays(1));
-    assertTrue(invoice.isOverdue());
-
-  }
-
-  @Test
   void invalidInvoice() {
     assertThrows(InvalidAmountException.class, () -> new Invoice("Test 3", 0, LocalDate.now()));
   }
@@ -29,4 +22,19 @@ public class InvoiceTest {
   void illegalArgument() {
     assertThrows(IllegalArgumentException.class, () -> new Invoice("Test 4", 90, null));
   }
+
+  @Test
+  void invoiceDueTodayIsOverdue() {
+    Invoice invoice = new Invoice("Test5", 500, LocalDate.now());
+    assertTrue(invoice.isOverdue());
+  }
+
+  @Test
+  void unpaidInvoiceDueDateYesterday() {
+    Invoice invoice = new Invoice("Test6", 1000, LocalDate.now().minusDays(1));
+    assertTrue(invoice.isOverdue());
+    invoice.markAsPaid();
+    assertFalse(invoice.isOverdue());
+  }
+
 }
